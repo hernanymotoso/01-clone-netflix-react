602f1f2956d7d0b3c5fd7dade9b527330a02cfe9
Hernany Dias <hd@globaltechstaff.com>
2022-01-09 16:54:10 -0300

    feat: i created initial structure

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  console.log('teste');
+
+  return <h1>teste</h1>;
+};
+export default Home;
---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button{
+    cursor: pointer;
+  }
+
+
+
+
+
+
+
+`;
